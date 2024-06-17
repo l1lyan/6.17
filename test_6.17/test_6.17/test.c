a136a53362da54a62f1091e287e99be01ad84585
l1lyan <124878185+l1lyan@users.noreply.github.com>
2024-06-17 11:54:50 +0800

    Add files via upload

---
+++ test_6.17/test_6.17/test.c
@@ -0,0 +1,45 @@
+
+#define _CRT_SECURE_NO_WARNINGS 1
+//#include<stdio.h>
+//
+//#include<math.h>
+//int main()
+//{
+//	int n = 0;
+//	scanf("%d", &n);
+//	int a = 0;
+//	int b = 1;
+//	int c = a + b;
+//	while (1)
+//	{
+//		if (n == b)
+//		{
+//			printf("0\n");
+//			break;
+//		}
+//		else if (n < b)
+//		{
+//			if (abs(a - n) > abs(b - n))
+//			{
+//				printf("%d\n", abs(b - n));
+//			}
+//			else
+//			{
+//				printf("%d\n", abs(a - n));
+//			}
+//			break;
+//		}
+//		c = a + b;
+//		a = b;
+//		b = c;
+//	}
+//	return 0;
+//}
+
+#include<stdio.h>
+
+int main()
+{
+
+	return 0;
+}
